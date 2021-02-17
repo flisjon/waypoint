0991e4abd22754df1ca21141449a2d1cd59c347f
Flisjon <49822587+flisjon@users.noreply.github.com>
2021-02-17 11:13:35 +1000

    finished book routes

--- routes/authors.js
+++ routes/authors.js
@@ -86,13 +86,13 @@ router.delete('/:id', async (req, res) => {
   let author
   try {
     author = await Author.findById(req.params.id)
-        await author.remove()
+    await author.remove()
     res.redirect('/authors')
   } catch {
     if (author == null) {
       res.redirect('/')
     } else {
-    res.render(`/authors/${author.id}`)
+      res.redirect(`/authors/${author.id}`)
     }
   }
 })
